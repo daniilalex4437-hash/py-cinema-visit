28e1c1f5b85b031db2541b8ed2f3002f7b2a3ae1
daniilalex4437-hash <daniilalex4437@gmail.com>
2026-05-07 00:47:54 +0200

    Refactor cinema_visit function and imports

--- app/main.py
+++ app/main.py
@@ -1,29 +1,29 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
+    movie: str,
     customers: list[dict],
     hall_number: int,
     cleaner: str,
-    movie: str,
 ) -> None:
-    customer_instances = [
-        Customer(name=customer["name"], food=customer["food"])
-        for customer in customers
+    customer_objects = [
+        Customer(name=c["name"], food=c["food"])
+        for c in customers
     ]
 
-    for customer in customer_instances:
+    cleaner_obj = Cleaner(cleaner)
+    hall = CinemaHall(hall_number)
+
+    for customer in customer_objects:
         CinemaBar.sell_product(product=customer.food, customer=customer)
 
-    cinema_hall = CinemaHall(hall_number=hall_number)
-    cleaning_staff = Cleaner(name=cleaner)
-
-    cinema_hall.movie_session(
+    hall.movie_session(
         movie_name=movie,
-        customers=customer_instances,
-        cleaning_staff=cleaning_staff,
+        customers=customer_objects,
+        cleaning_staff=cleaner_obj,
     )
     
